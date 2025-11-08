7c818097f769710694de71b07d9664b7009ec4cd
PilarTanco06 <pilartq@gmail.com>
2025-11-08 12:08:09 -0500

    Resolución conflicto de merge

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-namespace DondeComemos.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
-=======
 namespace DondeComemos.Models;
 
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
+    
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
->>>>>>> f90b87d81de3ce8c6b022ece9f01afa7f99a0eb7
